3503a5072f53d3896e6fe1fc62b2096b6ada40ae
Marcoantonio Manso <83654329+marcomansomm@users.noreply.github.com>
2023-05-26 12:54:10 -0300

    Tratando o envio das mensagens

--- consmessenger/consmessenger/src/main/java/com/example/consmessenger/Receiver.java
+++ consmessenger/consmessenger/src/main/java/com/example/consmessenger/Receiver.java
@@ -2,11 +2,13 @@ package com.example.consmessenger;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class Receiver {
 
-    public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+    public void receiveMessage(String[] message) {
+        System.out.println("[ "+ message[2] +" ] [ "+message[1]+" ] : ([SetorProduto]) precisamos de [ " + message[0] + " ] para [ "+ message[3]+ " ]");
     }
 
 
--- prodmessenger/prodmessenger/src/main/java/com/example/prodmessenger/Runner.java
+++ prodmessenger/prodmessenger/src/main/java/com/example/prodmessenger/Runner.java
@@ -1,10 +1,14 @@
 package com.example.prodmessenger;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
+import java.time.format.DateTimeFormatter;
+
 
 @Component
 public class Runner implements CommandLineRunner {
@@ -24,10 +28,21 @@ public class Runner implements CommandLineRunner {
 
         while(true) {
             System.out.println("Digite a mensagem:");
-            String msg;
-            msg = ler.nextLine();
+            String[] msg = new String[4];
+            LocalDateTime now = LocalDateTime.now();
+            // Definir o formato desejado da string
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            // Converter a data em uma string formatada
+            String dataFormatada = now.format(formatter);
 
-            if(msg.contains("sair"))
+            msg[0] = ler.nextLine();
+            msg[1] = ler.nextLine();
+            msg[2] = dataFormatada;
+            System.out.print("Digite a data (formato: dd/MM/yyyy): ");
+            String dataEstimada = ler.nextLine();
+            msg[3] = dataEstimada;
+
+            if(msg[0].contains("sair"))
                 break;
 
 
